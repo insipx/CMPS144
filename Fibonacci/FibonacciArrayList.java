866d9a7789cf234ef21af7ca5a7f49c59b22426d
Andrew Plaza <andrew.plaza15@gmail.com>
2015-09-20 19:04:47 -0400

    fibonacci number proj is done

--- Fibonacci/FibonacciArrayList.java
+++ Fibonacci/FibonacciArrayList.java
@@ -1,5 +1,5 @@
 /*
-Name:
+Name: Andrew Plaza
 
 Who did you help?
 
@@ -20,13 +20,33 @@ import javax.swing.*;
 public class FibonacciArrayList {
 
    public static ArrayList<Integer> Fibonacci (Integer Max){
-   /*
-   * Complete this method, meaningfully formatted
-   * Include in-line comments that explain each line of code
-   */
+
+	   
       ArrayList<Integer> A = new ArrayList<Integer>();
 
-
+      //we know that the first 2 fibonacci numbers have to be 1
+      	int a = 1;
+        int b = 1;
+        //we don't know the third value, so we will just leave it at 0 for now
+      	int c = 0;
+        //add the first two numbers to the list
+      	A.add(a);
+      	A.add(b);
+      	
+      	//I only want to calculate the fib numbers which are less than the upper bound(Max), so the program should stop if the latest fib number is > than Max
+      	while (c < Max){
+      		c = a +b;
+      		//We see that although the latest fib number is less than max, this will still calculate the next number which is greater than Max. Therefore make sure that c is less than the upper bound
+      		if (c < Max){
+      			A.add(c);
+      		}
+      		else{
+      			//if it' is more than upper bound don't do anything
+      		}
+      		//progress the fibonacci sequence by setting the values a and b to the next numbers in the sequence
+      		a = b;
+      		b = c;	
+      	}
 
       return A;
    }
@@ -39,10 +59,14 @@ public class FibonacciArrayList {
       max = new Integer(data);
       ArrayList<Integer> A = Fibonacci(max);
       System.out.println("There are " + A.size()+ " Fibonacci numbers less than "+max);
-      /*
-      * Add the code to complete the display of answers
-      */
+      //let's inform the user that the numbers being printed out are the fibonacci numbers
+      System.out.println("The Numbers Are: \n");
+      //iterate through the ArrayList A, one by one, printing each number out to the user
+      for (int i = 0; i< A.size(); i++){
+       System.out.println(A.get(i));	  
+      }
       
+     
       
    }
    
