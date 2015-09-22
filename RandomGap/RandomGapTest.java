21bc5c9df6a2c7cffae6e5912ef84ebe3ea03aee
Andrew Plaza <andrew.plaza15@gmail.com>
2015-09-22 01:36:26 -0400

    getting somewhere. def not finished. added two loops, one to to add to gap and one to check each number against the testArray holding the psuedo-randomly generated numbers. Constructed the Gap array in the constructor, which is hopefully what he wanted. Also hope he wants it to repeat 5000 times, which would makes sense since we are testing Java's number generator which is probably more intuitive than just some random num gen.

--- RandomGap/RandomGapTest.java
+++ RandomGap/RandomGapTest.java
@@ -1,7 +1,6 @@
 import java.util.*;
 /*
-Replace this line with your name
-
+Andrew Plaza
 Who did you help?
 
 
@@ -22,6 +21,7 @@ public class RandomGapTest {
 
    private Integer[] Uniform;
 
+   
 // Add attribute(s) to collect gap test data
 
    ArrayList<Integer> Gap;
@@ -32,13 +32,43 @@ public class RandomGapTest {
    public RandomGapTest(Integer range){
       this.range = range;
       Gap = new ArrayList<Integer>();
+      for(int i = 0; i < range; i++){
+    	  Gap.add(i, 0);
+    	  System.out.println("This is the range" + i);
+      }
       
    }
 
    public void RunTest(Integer Num){
-   
-   }
-   
+	   
+	   
+	  /// for (int repeat = 0; repeat < Num; repeat++){
+	   
+	   ArrayList<Integer> testArray = new ArrayList<Integer>();
+	      
+	   	  Random rand = new Random();
+	      for (int i=0; i< range; i++){
+	    	  testArray.add(rand.nextInt(range));
+	    	  System.out.println("And this is the testArray" + i);
+	      }
+	   //this is to loop through range abstractly
+	   for (int i = 0; i < Gap.size(); i++){
+		   int gapcount = 0;
+		   //this loops through the range to see if any gaps occur. This should measure frequency of gaps
+		   for(int j =0; j < testArray.size(); j++ ){
+	 		  
+	    		  if (testArray.get(i) == j){
+	    			  gapcount++;
+	    		  }
+	    		
+	    	  }
+		   
+		   Gap.add(i, Gap.get(i)+gapcount);
+		    
+	    	  
+	      }
+  // }
+}
    public ArrayList<Integer> GetGapData(){
       return this.Gap;
    }
