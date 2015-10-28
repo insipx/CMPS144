7e59244691d73f111c4fe4885e42fbf5351ffdbb
Andrew Plaza <andrew.plaza15@gmail.com>
2015-10-27 23:44:47 -0400

    finished greedy algorithm project done

--- GreedyAlgorithm/Greedy.java
+++ GreedyAlgorithm/Greedy.java
@@ -33,26 +33,55 @@
       }
    	
       public void go(){
-         Integer counter = 0;
-         Integer backup = 0;
+
          while(!Working.empty()){
          	// work on the item at the top of the working stack
-            NodeInfo Current = Working.peek();
-         
-         
-         
-         	// Insert this statement everytime you change the color of a map node
-            M.displayElement(Current.getName(), C[Current.getColor()], frame);
-				
-				
-				/*Move items from one stack to the other with statements like
-				*
-				*               Working.push(Valid.pop());
-				* and
-				*               Valid.push(Working.pop());
-				*
-				*/
-         	
+            NodeInfo Current = Working.peek();   
+            
+    
+            //set i to the current color -1
+            int i = Current.getColor() - 1;
+            
+            //Start from the fourth color
+            if(i<0) i = 4;
+            	//iterate through each color
+            	//while the color is greater than or equal to 0 keep going
+            	while(i >= 0){
+            		
+            		//set the color
+            		Current.setColor(i);
+            		//color the map
+            		M.displayElement(Current.getName(), C[Current.getColor()], frame);
+            		//if it's a valid coloring, we got it! 
+            		if(Current.validColoring()){
+            			
+            			break;
+            		}
+            		//else we don't and just color the map
+            		else{
+            			M.displayElement(Current.getName(), C[Current.getColor()], frame);
+            		}
+            		
+            		//increment the color, subtracting one
+            		i--;
+            	}
+            	//if the color falls through to 0, we have a problem
+            	if(Current.getColor() ==0){
+            		//put it on the working stack, we need to backup/do some more coloring work
+            		Working.push(Valid.pop());
+            		
+            		
+            	}else{
+            		//we got it right and can push it to the valid stack
+            		Valid.push(Working.pop());
+            		//make sure to color the state
+            		M.displayElement(Current.getName(), C[Current.getColor()], frame);
+            		
+            	}
+            	//color the state
+            	M.displayElement(Current.getName(), C[Current.getColor()], frame);
+            	 	            
+ 	
          }
       }
    	
