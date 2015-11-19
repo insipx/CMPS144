c8d5b9d798030ad9d31f75fac1c3a1c7e08e9723
Andrew Plaza <andrew.plaza15@gmail.com>
2015-11-19 18:49:14 -0500

    colored it right

--- BorderXing/BorderCrossing.java
+++ BorderXing/BorderCrossing.java
@@ -42,41 +42,51 @@
          //Is THIS the root???????
          NodeInfo start= G.refNode(A);
          NodeInfo Node = G.refNode(B);
-         start.setColor(4);
-         Node.setColor(4);
+         start.setColor(1);
+         Node.setColor(2);
+         M.displayElement(start.getName(), C[start.getColor()], frame);
+         M.displayElement(Node.getName(), C[Node.getColor()], frame);
          Q.insert(start);
          start.inQ();
          while (!Q.isEmpty() & !Finished){
             // Complete the code
             start = Q.remove();
-            start.setColor(3);
-            M.displayElement(start.getName(), C[start.getColor()], frame);
+            //start.setColor(3);
+            //M.displayElement(start.getName(), C[start.getColor()], frame);
             for(int i = 0; i < start.noOfNeighbors(); i++){
             	if (start.getNeighbor(i).getQ() != true){
+            		//mark the neighbor as 'used' or in the queue
             		start.getNeighbor(i).inQ();
-            		start.getNeighbor(i).setParent(start);            		
+            		//set the parent neighbor 
+            		start.getNeighbor(i).setParent(start);     
+            		//insert the neighbor into the queue
             		Q.insert(start.getNeighbor(i));
-            		start.getNeighbor(i).setColor(3);
             		//color the map blue
             		//start.getNeighbor(i).setColor(4);
                     //M.displayElement(start.getName(), C[start.getNeighbor(i).getColor()], frame);
                     
-                    if(start.getNeighbor(i) == Node){
+            		//if we reach the state we are looking for stop looking 
+            		//Color the neighbors
+            		start.getNeighbor(i).setColor(3);
+                    M.displayElement(start.getNeighbor(i).getName(), C[start.getNeighbor(i).getColor()], frame);
+            		if(start.getNeighbor(i) == Node){
                     	Finished = true;
+                    	
                     }
-
-                	
+                    //color thenode green
+            		
             	}
             	
-                M.displayElement(start.getName(), C[start.getNeighbor(i).getColor()], frame);
-
+                
+            	
             }
             
         //1 = red
             //3 == green
-        	
+            //color red
             start.setColor(1);
             M.displayElement(start.getName(), C[start.getColor()], frame);   
+            
             
          }
         
